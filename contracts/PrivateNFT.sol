5a2980fee81108bc6445a9f637d789a5528a5426
rahuldhakate740 <87521575+rahuldhakate740@users.noreply.github.com>
2024-09-03 08:16:44 +0000

    feat: initiated the project

--- contracts/PrivateNFT.sol
+++ contracts/PrivateNFT.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PrivateNFT is ERC721, ERC721Burnable, Ownable {
     constructor(address initialOwner)
-        ERC721("swissbroker","swb")
+        ERC721("Proxywar","prw")
         Ownable(initialOwner)
     {}
 
